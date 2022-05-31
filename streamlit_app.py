7a98b5929af2c454416f5c0221421e6515ad3805
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-05-31 13:26:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-# Draw a title and some text to the app:
+# Display text
+'Hello world'
+
+# Display a title and some text to the app:
 '''
 # This is the document title
 
@@ -12,7 +15,7 @@ df = pd.DataFrame({'col1': [1,2,3], 'col2': [4,5,6]})
 df  # 👈 Draw the dataframe
 
 x = 10
-'x', x  # 👈 Draw the string 'x' and then the value of x
+'x', x  # 👈 Display the string 'x' and then the value of x
 
 # Also works with most supported chart types
 import matplotlib.pyplot as plt
@@ -22,4 +25,4 @@ y = np.random.normal(0, 1, size=1000)
 fig, ax = plt.subplots()
 ax.hist(y, bins=20)
 
-fig  # 👈 Draw a Matplotlib chart
+fig  # 👈 Display a Matplotlib chart
